f05d2a126c6b013e65d67f74836e9d7174c334e1
Titouan B <nightbringer4ever@gmail.com>
2018-10-21 19:49:15 +0200

    test with GET single working

--- tests/IntlBooksTest.php
+++ tests/IntlBooksTest.php
@@ -26,6 +26,18 @@ class IntlBooksTest extends WebTestCase
         $this->assertStatusCode(200, $client);
         $this->assertEquals('Livre FR 0', $data['hydra:member'][0]['title']);
 
+
+        $client->request('GET', '/api/books/1',
+        array(),
+        array(),
+        array(
+            'HTTP_X-LOCALE' => 'fr',
+            'HTTP_Accept' => 'application/ld+json'
+        ));
+        $data = json_decode($client->getResponse()->getContent(), true);
+        $this->assertStatusCode(200, $client);
+        $this->assertEquals('Livre FR 0', $data['title']);
+
        
     }
 }
